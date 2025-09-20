a949cbda6c2e9769aa4bb886cd14533afe27f64a
RIOKOWI <achyarrio2004@gmail.com>
2025-09-20 19:09:00 +0700

    start

---
+++ bin/pzn_belajar_dart_dasar.dart
@@ -0,0 +1,5 @@
+import 'package:pzn_belajar_dart_dasar/pzn_belajar_dart_dasar.dart' as pzn_belajar_dart_dasar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pzn_belajar_dart_dasar.calculate()}!');
+}
---
+++ test/pzn_belajar_dart_dasar_test.dart
@@ -0,0 +1,8 @@
+import 'package:pzn_belajar_dart_dasar/pzn_belajar_dart_dasar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
