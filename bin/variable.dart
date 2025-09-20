338940bef5cbeb6afedfbc7781ae34c5c94d7474
RIOKOWI <achyarrio2004@gmail.com>
2025-09-20 20:00:51 +0700

    VARIABLE : const

--- bin/variable.dart
+++ bin/variable.dart
@@ -9,4 +9,22 @@ void main()
   String nama = 'mbut';
   print(nama);
 
+  // kata kunci var
+  var jeneng = 'popowi';
+  print(jeneng);
+
+  // (final) = untuk variabel tidak bisa diubah (masih bisa diubah)
+  final destination = 'roh';
+  print (destination);
+
+  // (const) = tidak bisa diubah sama sekali
+  final array1 = [1,2,3];
+  const array2 = [1,2,3];
+
+  array1[0] = 5;
+  // array1 = [0,0,0]; // tidak boleh kalo final
+  // array2[0] = 5; // cant't run/modified
+
+  print(array1);
+  print(array2);
 }
