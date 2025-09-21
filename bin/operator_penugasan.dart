001f6b434f9140fcded23709fe8b8b188285b438
RIOKOWI <achyarrio2004@gmail.com>
2025-09-21 10:06:39 +0700

    OPERATOR PENUGASAN : inc & dec

--- bin/operator_penugasan.dart
+++ bin/operator_penugasan.dart
@@ -3,11 +3,20 @@ void main()
   var a = 20;
 
   a += 10;
-  a -= 10;
-  a *= 10;
-  a ~/= 10;
-  a %= 10;
+  // a -= 10;
+  // a *= 10;
+  // a ~/= 10;
+  // a %= 10;
 
   print(a);
+
+  // incremet & decrement
+  var i = 5;
+  print(++i); // 6
+  print(i);   // 6
+
+  var j = 5;
+  print(j++); // 5
+  print(j);   // 6
 
 }
