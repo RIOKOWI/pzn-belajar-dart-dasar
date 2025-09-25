aa3368af31ffa95fd0b43847b28c6793a611a7c0
RIOKOWI <achyarrio2004@gmail.com>
2025-09-26 06:25:08 +0700

    ANONYMOUS FUNCTION : as parameter

--- bin/anonymous_function.dart
+++ bin/anonymous_function.dart
@@ -1,5 +1,11 @@
+void sayHello(String name, String Function(String) filter)
+{
+  print('hello ${filter(name)}');
+}
+
 void main()
 {
+
   // anonymous function
   var upperFunction = (String name) {
     return name.toUpperCase();
@@ -10,4 +16,13 @@ void main()
 
   print(upperFunction('rio'));
   print(lowerFunction('RIO'));
+
+
+  // anonymous function as parameter
+
+  sayHello('rio achyar', (name){
+    return name.toUpperCase();
+  });
+
+  sayHello('RIO ACHYAR', (name) => name.toLowerCase());
 }
